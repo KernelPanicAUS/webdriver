5b15d6f3b8cffe6bf8b11e5778f8d78b1aa9575c
Thomas Khalil <thomas@khalilt.com>
2016-09-28 11:24:58 +0200

    Formatting and shebang

--- geb-chrome-driver.groovy
+++ geb-chrome-driver.groovy
@@ -1,15 +1,14 @@
-
+#!/usr/bin/env groovy
 @Grapes([
     @Grab("org.codehaus.geb:geb-core:0.7.2"),
     @Grab("org.seleniumhq.selenium:selenium-chrome-driver:2.28.0"),
     @Grab("org.seleniumhq.selenium:selenium-support:2.28.0")
 ])
+
 import geb.Browser
 import org.openqa.selenium.chrome.ChromeDriver
 
-
 b = new Browser(driver: new ChromeDriver())
-
 
 b.drive {
     go "http://www.google.com"
